64f7e399292a17da77b58edc443d7be93547a46d
ming995 <hymghost@gmail.com>
2021-10-23 02:24:56 +0800

    feat: 增加一个新页面

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button, Card, Media, Image, Heading, Content } from 'react-bulma-components';
+import 'bulma/css/bulma.min.css';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Card style={{ width: 500, margin: 'auto' }}>
+      <Card.Image
+        size="4by3"
+        src="https://yanminghou.pek3b.qingstor.com/photo/WechatIMG30.jpeg"
+      />
+      <Card.Content>
+        <Media>
+          <Media.Item renderAs="figure" align="left">
+            <Image
+              size={64}
+              alt="64x64"
+              src="https://yanminghou.pek3b.qingstor.com/photo/WechatIMG30.jpeg"
+            />
+          </Media.Item>
+          <Media.Item>
+            <Heading size={4}>yanmingHou</Heading>
+            <Heading subtitle size={6}>
+              @insomniac
+            </Heading>
+          </Media.Item>
+        </Media>
+        <Content>
+          程序员/业余摄影师/2.5次元人类
+           <a>@insomniac</a>.<a href="https://github.com/ming995">#github</a>{' '}
+          <a href="https://500px.com.cn/yanminghou">#500px</a>
+          <br />
+        </Content>
+      </Card.Content>
+    </Card>
     </div>
   );
 }
