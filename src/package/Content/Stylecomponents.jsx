6a55e6f0164fe3d6d7981e145768b8f731be09be
ming995 <hymghost@gmail.com>
2021-11-09 09:15:47 +0800

    feat: 增加map显示

--- src/package/Content/Stylecomponents.jsx
+++ src/package/Content/Stylecomponents.jsx
@@ -2,10 +2,16 @@ import styled from "styled-components";
 
 const ContentWrapper = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
   font-size: 100px;
   color: #f8f8f8;
+  /* padding: 10px; */
+
+  .map-wrapper {
+    flex: 1;
+    background-color: #ffffff;
+    /* width: 700px; */
+    height: 900px;
+  }
 `;
 
 export default ContentWrapper;
--- src/package/Content/index.tsx
+++ src/package/Content/index.tsx
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown'
 import EasyTyper from 'easy-typer-js'
+import { Map } from 'react-amap';
 
 import ContentWrapper from './Stylecomponents';
 
 // import test from '../../doc/test.md';
 
 export default function Content(props: any) {
+    // 纬度
+    const [latitude, setLatitude] = useState<number>(0);
+    // 经度
+    const [longitude, setLongitude] = useState<number>(0);
 
 
+    const showPosition = (position: any) => {
+        let newLatitude = position.coords.latitude;
+        setLatitude(newLatitude);
+        let newLongitude = position.coords.longitude;
+        setLongitude(newLongitude);
+    }
+
+    const getLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        }
+    }
+
+    useEffect(() => {
+        getLocation();
+    }, [])
     return (
         <ContentWrapper>
             <ReactMarkdown># Hello, *world*!</ReactMarkdown>
+            <div className='map-wrapper'>
+                <Map center={[longitude, latitude]} />
+            </div>
         </ContentWrapper>
 
     )
